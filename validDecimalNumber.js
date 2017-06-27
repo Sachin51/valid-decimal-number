8636d4572035cac50558ef90d94997d459afcc6c
Sachin Kumar <ska261291@gmail.com>
2017-06-27 18:18:12 +0530

    JS file added.

---
+++ validDecimalNumber.js
@@ -0,0 +1,52 @@
+.directive('validDecimalNumber', function () {
+      return {
+        require: '?ngModel',
+        link: function (scope, element, attrs, ngModelCtrl) {
+          if (!ngModelCtrl) {
+            return;
+          }
+
+          var clean;
+          ngModelCtrl.$parsers.push(function (val) {
+            if (angular.isUndefined(val)) {
+              val = '';
+            }
+
+            if (attrs.decimal == 0) {
+              clean = val.replace(/[^-0-9]/g, '');
+            } else {
+              clean = val.replace(/[^-0-9\.]/g, '');
+            }
+
+
+            var negativeCheck = clean.split('-');
+            var decimalCheck = clean.split('.');
+            if (angular.isDefined(negativeCheck[1])) {
+              negativeCheck[1] = negativeCheck[1].slice(0, negativeCheck[1].length);
+              clean = negativeCheck[0] + '-' + negativeCheck[1];
+              if (negativeCheck[0].length > 0) {
+                clean = negativeCheck[0];
+              }
+
+            }
+
+            if (angular.isDefined(decimalCheck[1])) {
+              decimalCheck[1] = decimalCheck[1].slice(0, attrs.decimal);
+              clean = decimalCheck[0] + '.' + decimalCheck[1];
+            }
+
+            if (val !== clean) {
+              ngModelCtrl.$setViewValue(clean);
+              ngModelCtrl.$render();
+            }
+            return clean;
+          });
+
+          element.bind('keypress', function (event) {
+            if (event.keyCode === 32) {
+              event.preventDefault();
+            }
+          });
+        }
+      };
+    });
