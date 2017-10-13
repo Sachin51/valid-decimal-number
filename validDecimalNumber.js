9b2c298fdb68a72aa5b3fd70b2dfd29699b25b82
Sachin Kumar <ska261291@gmail.com>
2017-10-13 14:57:29 +0530

    Attribute to validate '+'/'-' numbers added.
    'positiveOnly' attribute added to validate positive and negative numbers.

--- validDecimalNumber.js
+++ validDecimalNumber.js
@@ -1,57 +1,65 @@
-(function(){
+(function() {
+  angular.module('valid.decimal.number', [])
 
-angular.module('valid.decimal.number', [])
-      
-.directive('validDecimalNumber', function () {
-      return {
-        require: '?ngModel',
-        link: function (scope, element, attrs, ngModelCtrl) {
-          if (!ngModelCtrl) {
-            return;
+  .directive('validDecimalNumber', function() {
+    return {
+      require: '?ngModel',
+      link: function(scope, element, attrs, ngModelCtrl) {
+        if (!ngModelCtrl) {
+          return;
+        }
+
+        var clean;
+        ngModelCtrl.$parsers.push(function(val) {
+          if (angular.isUndefined(val)) {
+            val = '';
           }
 
-          var clean;
-          ngModelCtrl.$parsers.push(function (val) {
-            if (angular.isUndefined(val)) {
-              val = '';
+           
+          if (attrs.positiveOnly == 'true') { //To accept only positive number '-' removed
+            if (attrs.decimal == 0) {
+              clean = val.replace(/[^0-9]/g, '');
+            } else {
+              clean = val.replace(/[^0-9\.]/g, '');
             }
-
+          } else {
             if (attrs.decimal == 0) {
               clean = val.replace(/[^-0-9]/g, '');
             } else {
               clean = val.replace(/[^-0-9\.]/g, '');
             }
+          }
 
 
-            var negativeCheck = clean.split('-');
-            var decimalCheck = clean.split('.');
-            if (angular.isDefined(negativeCheck[1])) {
-              negativeCheck[1] = negativeCheck[1].slice(0, negativeCheck[1].length);
-              clean = negativeCheck[0] + '-' + negativeCheck[1];
-              if (negativeCheck[0].length > 0) {
-                clean = negativeCheck[0];
-              }
-
+          var negativeCheck = clean.split('-');
+          var decimalCheck = clean.split('.');
+          if (angular.isDefined(negativeCheck[1])) {
+            negativeCheck[1] = negativeCheck[1].slice(0, negativeCheck[1].length);
+            clean = negativeCheck[0] + '-' + negativeCheck[1];
+            if (negativeCheck[0].length > 0) {
+              clean = negativeCheck[0];
             }
 
-            if (angular.isDefined(decimalCheck[1])) {
-              decimalCheck[1] = decimalCheck[1].slice(0, attrs.decimal);
-              clean = decimalCheck[0] + '.' + decimalCheck[1];
-            }
+          }
 
-            if (val !== clean) {
-              ngModelCtrl.$setViewValue(clean);
-              ngModelCtrl.$render();
-            }
-            return clean;
-          });
+          if (angular.isDefined(decimalCheck[1])) {
+            decimalCheck[1] = decimalCheck[1].slice(0, attrs.decimal);
+            clean = decimalCheck[0] + '.' + decimalCheck[1];
+          }
 
-          element.bind('keypress', function (event) {
-            if (event.keyCode === 32) {
-              event.preventDefault();
-            }
-          });
-        }
-      };
-    });
+          if (val !== clean) {
+            ngModelCtrl.$setViewValue(clean);
+            ngModelCtrl.$render();
+          }
+          return clean;
+        });
+
+        element.bind('keypress', function(event) {
+          if (event.keyCode === 32) {
+            event.preventDefault();
+          }
+        });
+      }
+    };
+  });
 })();
